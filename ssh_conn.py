d24719bc2e7321793d026c6fff53441cdc809657
oa1321 <74298257+oa1321@users.noreply.github.com>
2022-05-12 11:38:23 +0300

    Add files via upload

--- ssh_conn.py
+++ ssh_conn.py
@@ -1,14 +1,32 @@
 from scapy.all import *
 from collections import Counter
-
+import threading
+import os
+from time import sleep
 pkt_per_sec = 0
 
 def timer_func():
-    pkt_per_sec = 0 
-    sleep(1)
-    if pkt_per_sec > 20:
-        print("[+] Fuzzing detected")#its a alot of pkt that was sent and probably fuzzing
-        exit(0)
+    while True:
+        pkt_per_sec = 0 
+        sleep(1)
+        if pkt_per_sec > 20:
+            print("[+] Fuzzing detected")#its a alot of pkt that was sent and probably fuzzing
+            exit(0)
+
+def auth_r():
+    with open("/var/log/auth.log", "r") as f:
+        #find the last line of the file
+        f.seek(0,os.SEEK_END)
+        #check for errors in auto.log file in the last 5 lines
+        while True:
+            line = f.readline()
+            if "error" in line.lower():
+                print("[+] Fuzzing detected")
+                exit(0)
+            if "invalid user" in line.lower():
+                print("[+] Fuzzing detected")
+                exit(0)
+
 
 def filter_packets(pkt):
     #checks if the packet is an SSH packet
@@ -18,6 +36,8 @@ def filter_packets(pkt):
 		    #ubuntu debian redhat centos fedora windows Windows Server UNIX OS 
 			#check the src send frequency if too high then drop the packet
             str_p = str(pkt[Raw].load)
+            pkt_per_sec += 1
+            print(str_p)
             runner = 0 
             while(runner < len(str_p)-7):
                 temp_str = str_p[runner:runner+7]
@@ -26,18 +46,11 @@ def filter_packets(pkt):
                     if count[key] > 5:
                         print("[+] Fuzzing detected")
                         exit(0)
-
-            #read auth.log and check for errors
-            with open("/var/log/auth.log", "r") as f:
-                #check for errors in auto.log file in the last 5 lines
-                for line in f.readlines()[-1]:
-                    if "error" in line.lower():
-                        print("[+] Fuzzing detected")
-                        exit(0)
-                    if "invalid user" in line.lower():
-                        print("[+] Fuzzing detected")
-                        exit(0)
-
             print("[-] Fuzzing not detected")
+
+thred = threading.Thread(target=timer_func)
+thred.start()
+thred2 = threading.Thread(target=auth_r)
+thred2.start()
 
 pkt = sniff(filter="port 22", prn=filter_packets)
